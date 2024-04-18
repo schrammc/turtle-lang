7fdc45b5a5da0f00eb588ab4aed4c4ccbaabe838
Christof Schramm <public@christof-schramm.net>
2024-04-18 23:07:46 +0200

    Add barebones executable with optparse applicative

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,41 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+
 module Main (main) where
 
-import Lib
+import qualified Data.Text.IO as T
+import Language.Turtle.Frontend.Lexer
+import Language.Turtle.Frontend.Parser
+import Options.Applicative
+import System.Exit (exitFailure)
+
+data ParseOptions = ParseOptions {filePath :: String}
+
+data Command = Parse ParseOptions
+
+parseCommand :: Parser Command
+parseCommand = Parse . ParseOptions <$> argument str (metavar "FILE")
+
+oparse :: ParserInfo Command
+oparse =
+    info
+        ( helper
+            <*> subparser
+                ( command "parse" (info parseCommand (progDesc "Parse some turtle-lang code"))
+                )
+        )
+        (fullDesc <> progDesc "The Turtle programming language" <> header "turtle")
 
 main :: IO ()
-main = someFunc
+main = do
+    execParser oparse >>= \case
+        Parse opts -> runParse opts
+
+runParse :: ParseOptions -> IO ()
+runParse opts = do
+    fileContent <- T.readFile $ opts.filePath
+    let parseResult = runAlex fileContent program
+    case parseResult of
+        Right res -> print res
+        Left err -> do
+            putStrLn $ "ERROR: " ++ err
+            exitFailure
