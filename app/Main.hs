63e5db792f761b93412dd3ab12912aef0ae65063
Christof Schramm <public@christof-schramm.net>
2024-05-08 00:01:50 +0200

    Subcommand for tokenizing

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,38 @@
+{-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedRecordDot #-}
 
 module Main (main) where
 
 import Data.Text as T
 import qualified Data.Text.IO as T
+import Data.Text.Lazy (toChunks)
 import Language.Turtle.Error (TurtleError (..), parserError, renderErrorWithFileContent)
 import Language.Turtle.Frontend.Lexer
+import Language.Turtle.Frontend.Lexer (tokenize)
 import Language.Turtle.Frontend.Parser
 import Language.Turtle.Location (Location (..))
 import Options.Applicative
 import System.Exit (exitFailure)
 
 data ParseOptions = ParseOptions {filePath :: String}
-
-data Command = Parse ParseOptions
+data TokenizeOptions = TokenizeOptions {filePath :: String}
+data Command
+    = Parse ParseOptions
+    | Tokenize TokenizeOptions
 
 parseCommand :: Parser Command
 parseCommand = Parse . ParseOptions <$> argument str (metavar "FILE")
+
+tokenizeCommand :: Parser Command
+tokenizeCommand = Tokenize . TokenizeOptions <$> argument str (metavar "FILE")
 
 oparse :: ParserInfo Command
 oparse =
     info
         ( helper
-            <*> subparser
+            <*> hsubparser
                 ( command "parse" (info parseCommand (progDesc "Parse some turtle-lang code"))
+                    <> command "tokenize" (info tokenizeCommand (progDesc "Tokenize turtle-lang code"))
                 )
         )
         (fullDesc <> progDesc "The Turtle programming language" <> header "turtle")
@@ -32,6 +41,7 @@ main :: IO ()
 main = do
     execParser oparse >>= \case
         Parse opts -> runParse opts
+        Tokenize opts -> runTokenize opts
 
 runParse :: ParseOptions -> IO ()
 runParse opts = do
@@ -49,3 +59,9 @@ runParse opts = do
                         }
             T.putStrLn $ renderErrorWithFileContent turtleError fileContent
             exitFailure
+runTokenize :: TokenizeOptions -> IO ()
+runTokenize opts = do
+    fileContent <- T.readFile $ opts.filePath
+    case tokenize fileContent of
+        Right tks -> mapM_ print tks
+        Left err -> putStrLn err >> exitFailure
