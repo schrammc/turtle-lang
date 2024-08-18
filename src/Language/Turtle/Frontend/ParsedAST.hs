a406f5ef9ccaeb63a232c9120eea66d7011f46d8
Christof Schramm <public@christof-schramm.net>
2024-08-18 21:36:25 +0200

    No generics-sop

--- src/Language/Turtle/Frontend/ParsedAST.hs
+++ src/Language/Turtle/Frontend/ParsedAST.hs
@@ -25,24 +25,17 @@ import Data.Kind (Type)
 import Data.List.NonEmpty
 import Data.Text (Text)
 import GHC.Generics (Generic)
-import Generics.SOP.TH
 
 data Literal
     = NumLit Double
     | StringLit Text
     deriving (Show, Eq)
 
-deriveGeneric (''Literal)
-
 newtype Ident = Ident Text
     deriving (Show, Eq)
 
-deriveGeneric (''Ident)
-
 newtype TypeIdent = TypeIdent Text
     deriving (Show, Eq)
-
-deriveGeneric (''TypeIdent)
 
 newtype FunctionParams = FunctionParams [ASTType]
     deriving (Show, Eq)
@@ -50,12 +43,9 @@ data ASTType
     = ASTType TypeIdent
     | FuncType FunctionParams ASTType
     deriving (Show, Eq)
-deriveGeneric (''FunctionParams)
-deriveGeneric (''ASTType)
 
 data IdentWithType = IdentWithType Ident ASTType
     deriving (Show, Eq)
-deriveGeneric (''IdentWithType)
 
 data Expression (f :: Type -> Type)
     = ELiteral Literal
@@ -63,8 +53,6 @@ data Expression (f :: Type -> Type)
     | EList [f (Expression f)]
     | ECall (f (Expression f)) [f (Expression f)]
     deriving (Generic)
-
-deriveGeneric (''Expression)
 
 class HFunctor t f where
     ffmap :: (Functor f) => (forall a. f a -> g a) -> t f -> t g
@@ -89,8 +77,6 @@ instance HFunctor Pattern a where
     ffmap f (VarPattern x) = VarPattern (f x)
     ffmap _ (LitPattern l) = LitPattern l
 
-deriveGeneric (''Pattern)
-
 deriving instance (Show1 f) => Show (Pattern f)
 deriving instance (Eq1 f) => Eq (Pattern f)
 
@@ -100,8 +86,6 @@ data Statement (f :: Type -> Type)
     | If (f (Expression f)) (NonEmpty (f (Statement f))) (NonEmpty (f (Statement f)))
     | FunDecl (f Ident) [f IdentWithType] (f ASTType) (NonEmpty (f (Statement f)))
     | MatchCase (f (Expression f)) [f (Pattern f, NonEmpty (f (Statement f)))]
-
-deriveGeneric (''Statement)
 
 instance HFunctor Statement a where
     ffmap f (Assignment i e) = Assignment (f i) (mapffmap f e)
